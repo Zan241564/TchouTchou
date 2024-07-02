c77bc0cf24a198b2a5f889e73c59d2c17522f3a1
Zan241564 <rzcazenave@gmail.com>
2024-07-02 20:20:49 +0200

    Infinite background scrolling
    Now duplicating biome background and replacing it in the front when it scrolls out of the camera.
    (all placeholder and harcoded for now, but the principle works)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,18 +9,32 @@ public class GameManager : MonoBehaviour
     Train _train;
     [SerializeField]
     Biome _biome;
+    Biome _biomeDupe;
     [SerializeField]
     float _trainVisualSpeed; // multiply the the scrolling speed of the background to create an illusion of speed for the train
+    [SerializeField]
+    float _testBiomeZScrollLimit;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _biomeDupe = Instantiate(_biome, new Vector3(-5.0f, 4.0f, 50.0f), Quaternion.identity);
+        _biomeDupe.transform.Rotate(90.0f, 0.0f, -90.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         _biome.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed;
+        _biomeDupe.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed;
+        if(_biome.transform.position.z < _testBiomeZScrollLimit)
+        {
+            _biome.transform.position = _biomeDupe.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+        }
+        if (_biomeDupe.transform.position.z < _testBiomeZScrollLimit)
+        {
+            _biomeDupe.transform.position = _biome.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+        }
+
     }
 }
