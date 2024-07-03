cafd317d8c25172bad428b339a533e03dc2236c7
Zan241564 <rzcazenave@gmail.com>
2024-07-03 14:26:05 +0200

    Separating ground and background
    The ground now has its separate scrolling speed to reinforce the paralax illusion.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,38 +8,29 @@ using UnityEditor.UIElements;
 public class GameManager : MonoBehaviour
 {
 
-    [SerializeField]
-    Train _train;
-    [SerializeField]
-    Biome _biome;
+    [SerializeField] Train _train;
+    [SerializeField] Biome _biome;
     int _currentBiomeID;
     Biome _biomeDupe;
-    [SerializeField]
-    float _trainVisualSpeedMemo;
+    [SerializeField] GameObject _ground;
+    GameObject _groundDupe;
+    [SerializeField] float _groundSpeedFactor;
+    [SerializeField] float _trainVisualSpeedMemo;
     float _trainVisualSpeed; // multiply the the scrolling speed of the background to create an illusion of speed for the train
-    [SerializeField]
-    float _trainAccelerationFactor; // also used for deceleration
+    [SerializeField] float _trainAccelerationFactor; // also used for deceleration
     bool _trainStop = false;
-    [SerializeField]
-    float _testBiomeZScrollLimit;
-    [SerializeField]
-    UnityEngine.UIElements.Button _startStop;
-    [SerializeField]
-    UnityEngine.UI.Image _fadeToBlack;
-    [SerializeField]
-    float _fadeToBlackSpeed;
+    [SerializeField] float _testBiomeZScrollLimit;
+    [SerializeField] UnityEngine.UIElements.Button _startStop;
+    [SerializeField] UnityEngine.UI.Image _fadeToBlack;
+    [SerializeField] float _fadeToBlackSpeed;
     bool _swappingBiome = false;
     float _biomeSwappingTimer;
-    [SerializeField]
-    Material _nextBiome;
-    [SerializeField]
-    Material _nextBiomeGround;
+    [SerializeField] Material _nextBiome;
+    [SerializeField] Material _nextBiomeGround;
     int _nextBiomeID = 1;
-    [SerializeField]
-    GameObject _clouds;
+    [SerializeField] GameObject _clouds;
     GameObject _cloudsDupe;
-    [SerializeField]
-    float _cloudsSpeedFactor;
+    [SerializeField] float _cloudsSpeedFactor;
 
     // Start is called before the first frame update
     void Start()
@@ -48,6 +39,9 @@ public class GameManager : MonoBehaviour
         _biomeDupe.transform.Rotate(90.0f, 0.0f, -90.0f);
         _trainVisualSpeed = _trainVisualSpeedMemo;
         _currentBiomeID = 0;
+        Vector3 groundDupePos = _ground.gameObject.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+        _groundDupe =Instantiate(_ground, groundDupePos, Quaternion.identity);
+        _groundDupe.gameObject.transform.Rotate(0.0f, 0.0f, 0.0f);
         Vector3 cloudsDupePos = _clouds.gameObject.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
         _cloudsDupe = Instantiate(_clouds, cloudsDupePos, Quaternion.identity);
         _cloudsDupe.gameObject.transform.Rotate(0.0f, 0.0f, -90.0f);
@@ -56,29 +50,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _biome.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed;
-        _biomeDupe.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed;
-        _clouds.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed * _cloudsSpeedFactor;
-        _cloudsDupe.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed * _cloudsSpeedFactor;
-        if (_biome.transform.position.z < _testBiomeZScrollLimit)
-        {
-            _biome.transform.position = _biomeDupe.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
-        }
-        if (_biomeDupe.transform.position.z < _testBiomeZScrollLimit)
-        {
-            _biomeDupe.transform.position = _biome.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
-        }
-        if(_clouds.transform.position.z < _testBiomeZScrollLimit)
-        {
-            Debug.Log("_clouds respawn");
-            _clouds.transform.position = _cloudsDupe.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
-        }
-        if (_cloudsDupe.transform.position.z < _testBiomeZScrollLimit)
-        {
-            Debug.Log("_cloudsDupe respawn");
-            _cloudsDupe.transform.position = _clouds.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
-        }
-
+        SceneryScrolling();
         if (_trainStop)
         {
             _trainVisualSpeed -= 1.0f * Time.deltaTime * _trainAccelerationFactor;
@@ -110,8 +82,8 @@ public class GameManager : MonoBehaviour
                 {
                     _biome.GetComponent<MeshRenderer>().material = _nextBiome;
                     _biomeDupe.GetComponent<MeshRenderer>().material = _nextBiome;
-                    _biome.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material = _nextBiomeGround;
-                    _biomeDupe.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material = _nextBiomeGround;
+                    _ground.GetComponent<MeshRenderer>().material = _nextBiomeGround;
+                    _groundDupe.GetComponent<MeshRenderer>().material = _nextBiomeGround;
                     _currentBiomeID = _nextBiomeID;
                 }
                 Color temp = _fadeToBlack.color;
@@ -122,6 +94,40 @@ public class GameManager : MonoBehaviour
             {
                 _swappingBiome = false;
             }
+        }
+    }
+
+    private void SceneryScrolling()
+    {
+        _biome.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed;
+        _biomeDupe.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed;
+        _ground.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed * _groundSpeedFactor;
+        _groundDupe.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed * _groundSpeedFactor;
+        _clouds.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed * _cloudsSpeedFactor;
+        _cloudsDupe.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed * _cloudsSpeedFactor;
+        if (_biome.transform.position.z < _testBiomeZScrollLimit)
+        {
+            _biome.transform.position = _biomeDupe.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+        }
+        if (_biomeDupe.transform.position.z < _testBiomeZScrollLimit)
+        {
+            _biomeDupe.transform.position = _biome.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+        }
+        if (_ground.transform.position.z < _testBiomeZScrollLimit)
+        {
+            _ground.transform.position = _groundDupe.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+        }
+        if (_groundDupe.transform.position.z < _testBiomeZScrollLimit)
+        {
+            _groundDupe.transform.position = _ground.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+        }
+        if (_clouds.transform.position.z < _testBiomeZScrollLimit)
+        {
+            _clouds.transform.position = _cloudsDupe.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+        }
+        if (_cloudsDupe.transform.position.z < _testBiomeZScrollLimit)
+        {
+            _cloudsDupe.transform.position = _clouds.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
         }
     }
 
@@ -140,6 +146,11 @@ public class GameManager : MonoBehaviour
     public int GetBiomeID()
     {
         return _currentBiomeID;
+    }
+
+    public void SetNextBiomeID(int nextBiomeID)
+    {
+        _nextBiomeID = nextBiomeID;
     }
 
     // This is called automatically after a while when a new biome is loaded to stop the train and start the "management part" of the gameplay loop
