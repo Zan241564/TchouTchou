9b9383564bb79a6826e81a74eb54f78d47e53be4
Zan241564 <rzcazenave@gmail.com>
2024-07-04 00:55:13 +0200

    Actual game graphics
    Change placeholders for actual textures, changed train from a cube to a plane since it's just a texture, added rails (I've heard that could be usefull), implemented functions to actually change the next biome (not hard set anymore). I think that's all?...
    I'm not putting the textures in the commit. They're quite heavy.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,10 +8,10 @@ using UnityEditor.UIElements;
 public class GameManager : MonoBehaviour
 {
 
-    [SerializeField] Train _train;
-    [SerializeField] Biome _biome;
+    [SerializeField] GameObject _train;
+    [SerializeField] GameObject _biome;
     int _currentBiomeID;
-    Biome _biomeDupe;
+    GameObject _biomeDupe;
     [SerializeField] GameObject _ground;
     GameObject _groundDupe;
     [SerializeField] float _groundSpeedFactor;
@@ -25,27 +25,35 @@ public class GameManager : MonoBehaviour
     [SerializeField] float _fadeToBlackSpeed;
     bool _swappingBiome = false;
     float _biomeSwappingTimer;
+    [SerializeField] float _swappingBiomeExitingDuration;
+    [SerializeField] float _swappingBiomeTransitionDuration;
+    [SerializeField] float _swappingBiomeEnteringDuration;
     [SerializeField] Material _nextBiome;
     [SerializeField] Material _nextBiomeGround;
     int _nextBiomeID = 1;
+    [SerializeField] GameObject _sky;
     [SerializeField] GameObject _clouds;
     GameObject _cloudsDupe;
     [SerializeField] float _cloudsSpeedFactor;
-    [SerializeField] BiomeData _biomeData;
+    [SerializeField] BiomeData _forestData;
+    [SerializeField] BiomeData _desertData;
+    [SerializeField] BiomeData _mountainData;
+    BiomeData _currentBiomeData;
 
     // Start is called before the first frame update
     void Start()
     {
-        _biomeDupe = Instantiate(_biome, new Vector3(-5.0f, 4.0f, 50.0f), Quaternion.identity);
+        _currentBiomeData = _forestData;
+        _biomeDupe = Instantiate(_biome, new Vector3(-5.0f, 5.0f, 120.0f), Quaternion.identity);
         _biomeDupe.transform.Rotate(90.0f, 0.0f, -90.0f);
         _trainVisualSpeed = _trainVisualSpeedMemo;
         _currentBiomeID = 0;
-        Vector3 groundDupePos = _ground.gameObject.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
-        _groundDupe =Instantiate(_ground, groundDupePos, Quaternion.identity);
-        _groundDupe.gameObject.transform.Rotate(0.0f, 0.0f, 0.0f);
-        Vector3 cloudsDupePos = _clouds.gameObject.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+        Vector3 groundDupePos = _ground.gameObject.transform.position + new Vector3(0.0f, 0.0f, 120.0f);
+        _groundDupe = Instantiate(_ground, groundDupePos, Quaternion.identity);
+        _groundDupe.gameObject.transform.Rotate(90.0f, 0.0f, -90.0f);
+        Vector3 cloudsDupePos = _clouds.gameObject.transform.position + new Vector3(0.0f, 0.0f, 120.0f);
         _cloudsDupe = Instantiate(_clouds, cloudsDupePos, Quaternion.identity);
-        _cloudsDupe.gameObject.transform.Rotate(0.0f, 0.0f, -90.0f);
+        _cloudsDupe.gameObject.transform.Rotate(90.0f, 0.0f, -90.0f);
     }
 
     // Update is called once per frame
@@ -69,27 +77,27 @@ public class GameManager : MonoBehaviour
         _cloudsDupe.transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime * _trainVisualSpeed * _cloudsSpeedFactor;
         if (_biome.transform.position.z < _testBiomeZScrollLimit)
         {
-            _biome.transform.position = _biomeDupe.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+            _biome.transform.position = _biomeDupe.transform.position + new Vector3(0.0f, 0.0f, 120.0f);
         }
         if (_biomeDupe.transform.position.z < _testBiomeZScrollLimit)
         {
-            _biomeDupe.transform.position = _biome.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+            _biomeDupe.transform.position = _biome.transform.position + new Vector3(0.0f, 0.0f, 120.0f);
         }
         if (_ground.transform.position.z < _testBiomeZScrollLimit)
         {
-            _ground.transform.position = _groundDupe.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+            _ground.transform.position = _groundDupe.transform.position + new Vector3(0.0f, 0.0f, 120.0f);
         }
         if (_groundDupe.transform.position.z < _testBiomeZScrollLimit)
         {
-            _groundDupe.transform.position = _ground.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+            _groundDupe.transform.position = _ground.transform.position + new Vector3(0.0f, 0.0f, 120.0f);
         }
         if (_clouds.transform.position.z < _testBiomeZScrollLimit)
         {
-            _clouds.transform.position = _cloudsDupe.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+            _clouds.transform.position = _cloudsDupe.transform.position + new Vector3(0.0f, 0.0f, 120.0f);
         }
         if (_cloudsDupe.transform.position.z < _testBiomeZScrollLimit)
         {
-            _cloudsDupe.transform.position = _clouds.transform.position + new Vector3(0.0f, 0.0f, 50.0f);
+            _cloudsDupe.transform.position = _clouds.transform.position + new Vector3(0.0f, 0.0f, 120.0f);
         }
     }
 
@@ -116,29 +124,45 @@ public class GameManager : MonoBehaviour
     private void BiomeSwap()
     {
         _biomeSwappingTimer -= 1.0f * Time.deltaTime;
-        if (_biomeSwappingTimer > 2.5f)
+        if(_biomeSwappingTimer > _swappingBiomeTransitionDuration + _swappingBiomeEnteringDuration)
         {
+            // do exiting messages things
+        }
+        else if (_biomeSwappingTimer > _swappingBiomeTransitionDuration*0.5f + _swappingBiomeEnteringDuration)
+        {
+            // stop exiting messages and do the fade to black
             Color temp = _fadeToBlack.color;
             temp.a += 1.0f * Time.deltaTime * _fadeToBlackSpeed;
             _fadeToBlack.color = temp;
         }
-        else if (_biomeSwappingTimer > 0.0f)
+        else if (_biomeSwappingTimer > _swappingBiomeEnteringDuration)
         {
+            // do the switcheroo then undo the fade to black
             if (_currentBiomeID != _nextBiomeID)
             {
-                _biome.GetComponent<MeshRenderer>().material = _nextBiome;
-                _biomeDupe.GetComponent<MeshRenderer>().material = _nextBiome;
-                _ground.GetComponent<MeshRenderer>().material = _nextBiomeGround;
-                _groundDupe.GetComponent<MeshRenderer>().material = _nextBiomeGround;
+                Debug.Log(_currentBiomeData);
+                _sky.gameObject.GetComponent<MeshRenderer>().material.mainTexture = _currentBiomeData.sky;
+                _clouds.gameObject.GetComponent<MeshRenderer>().material.mainTexture = _currentBiomeData.clouds;
+                _cloudsDupe.gameObject.GetComponent<MeshRenderer>().material.mainTexture = _currentBiomeData.clouds;
+                _biome.GetComponent<MeshRenderer>().material.mainTexture =  _currentBiomeData.background;
+                _biomeDupe.GetComponent<MeshRenderer>().material.mainTexture = _currentBiomeData.background;
+                _ground.GetComponent<MeshRenderer>().material.mainTexture = _currentBiomeData.ground;
+                _groundDupe.GetComponent<MeshRenderer>().material.mainTexture = _currentBiomeData.ground;
+                _train.gameObject.GetComponent<MeshRenderer>().material.mainTexture = _currentBiomeData.train;
                 _currentBiomeID = _nextBiomeID;
             }
             Color temp = _fadeToBlack.color;
             temp.a -= 1.0f * Time.deltaTime * _fadeToBlackSpeed;
             _fadeToBlack.color = temp;
         }
+        else if (_biomeSwappingTimer > 0.0f)
+        {
+            // do the entering messages stuff
+        }
         else
         {
             _swappingBiome = false;
+            StopTrain();
         }
     }
 
@@ -149,9 +173,9 @@ public class GameManager : MonoBehaviour
 
     public void ChangeBiome()
     {
-        _swappingBiome = true;
-        _biomeSwappingTimer = 5.0f;
-        // set value of _nextBiome and _nextBiomeID. It's hard set for now.
+        //_swappingBiome = true;
+        //_biomeSwappingTimer = 5.0f;
+        StartTrain(1);
     }
 
     public int GetBiomeID()
@@ -159,7 +183,7 @@ public class GameManager : MonoBehaviour
         return _currentBiomeID;
     }
 
-    public void SetNextBiomeID(int nextBiomeID)
+    private void SetNextBiomeID(int nextBiomeID)
     {
         _nextBiomeID = nextBiomeID;
     }
@@ -171,10 +195,27 @@ public class GameManager : MonoBehaviour
         // do things
     }
 
-    // This is called when the player chooses the next biome to go. It calls all the "narrative part" of the gameplay loop
-    public void StartTrain()
+    // This is called when the player chooses the next biome to go. It calls all the "narrative part" of the gameplay loop and the biome swapping logic.
+    public void StartTrain(int nextBiome)
     {
+        SetNextBiomeID(nextBiome);
         _trainStop = false;
-        
+        _swappingBiome = true;
+        _biomeSwappingTimer = _swappingBiomeExitingDuration + _swappingBiomeTransitionDuration + _swappingBiomeEnteringDuration;
+        switch (_nextBiomeID)
+        {
+            case 0:
+                _currentBiomeData = _forestData;
+                break;
+            case 1:
+                _currentBiomeData = _desertData;
+                break;
+            case 2:
+                _currentBiomeData = _mountainData;
+                break;
+            default:
+                _currentBiomeData = _forestData;
+                break;
+        }
     }
 }
